nemomobile-graveyard/nemo-qml-plugins

[Email] - Update accountListModel signals.

email/src/emailaccountlistmodel.cpp
@@ -99,22 +99,32 @@ QVariant EmailAccountListModel::data(const QModelIndex &index, int role) const
 
 void EmailAccountListModel::onAccountsAdded(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
-    emit accountAdded(QVariant(ids[0]));
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountAdded(accountIds);
 }
 
 void EmailAccountListModel::onAccountsRemoved(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
-    emit accountRemoved(QVariant(ids[0]));
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountRemoved(accountIds);
 }
 
 void EmailAccountListModel::onAccountsUpdated(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountsUpdated(accountIds);
 }
 
 int EmailAccountListModel::indexFromAccountId(QVariant id)

email/src/emailaccountlistmodel.h
@@ -49,8 +49,9 @@ public slots:
     Q_INVOKABLE QDateTime lastUpdatedAccountTime();
 
 signals:
-    void accountAdded(QVariant accountId);
-    void accountRemoved(QVariant accountId);
+    void accountAdded(QVariantList accountIds);
+    void accountRemoved(QVariantList accountIds);
+    void accountsUpdated(QVariantList accountIds);
     void modelReset();
 
 private slots:

nemomobile/nemo-qml-plugin-email

src/emailaccountlistmodel.cpp
@@ -99,22 +99,32 @@ QVariant EmailAccountListModel::data(const QModelIndex &index, int role) const
 
 void EmailAccountListModel::onAccountsAdded(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
-    emit accountAdded(QVariant(ids[0]));
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountsAdded(accountIds);
 }
 
 void EmailAccountListModel::onAccountsRemoved(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
-    emit accountRemoved(QVariant(ids[0]));
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountsRemoved(accountIds);
 }
 
 void EmailAccountListModel::onAccountsUpdated(const QMailAccountIdList &ids)
 {
-    Q_UNUSED(ids);
     QMailAccountListModel::reset();
+    QVariantList accountIds;
+    foreach (QMailAccountId accountId, ids) {
+        accountIds.append(accountId.toULongLong());
+    }
+    emit accountsUpdated(accountIds);
 }
 
 int EmailAccountListModel::indexFromAccountId(QVariant id)

src/emailaccountlistmodel.h
@@ -49,8 +49,9 @@ public slots:
     Q_INVOKABLE QDateTime lastUpdatedAccountTime();
 
 signals:
-    void accountAdded(QVariant accountId);
-    void accountRemoved(QVariant accountId);
+    void accountsAdded(QVariantList accountIds);
+    void accountsRemoved(QVariantList accountIds);
+    void accountsUpdated(QVariantList accountIds);
     void modelReset();
 
 private slots: