3955f66724b88e07d9c6783e020691285a154e83
Valerio Valerio <valerio.valerio@jollamobile.com>
2014-05-19 13:28:25 +0300

    [nemo-qml-plugin-email] Add invokable standardFolderIndex(FolderType)

--- rpm/nemo-qml-plugin-email-qt5.spec
+++ rpm/nemo-qml-plugin-email-qt5.spec
@@ -9,7 +9,7 @@ Name:       nemo-qml-plugin-email-qt5
 # << macros
 
 Summary:    Email plugin for Nemo Mobile
-Version:    0.1.35
+Version:    0.1.36
 Release:    1
 Group:      System/Libraries
 License:    BSD
--- src/folderlistmodel.h
+++ src/folderlistmodel.h
@@ -64,6 +64,7 @@ public:
     Q_INVOKABLE int indexFromFolderId(int folderId);
     Q_INVOKABLE int numberOfFolders();
     Q_INVOKABLE void setAccountKey(int id);
+    Q_INVOKABLE int standardFolderIndex(FolderStandardType folderType);
 
 signals:
     void currentFolderIdxChanged();
